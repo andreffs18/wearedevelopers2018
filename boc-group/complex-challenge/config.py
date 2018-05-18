2c62a01a40705761f53063351adcc0d5bf6a0b36
andreffs18 <andreffs18@gmail.com>
2018-05-18 17:10:24 +0100

    Add flask-dotenv

--- boc-group/complex-challenge/config.py
+++ boc-group/complex-challenge/config.py
@@ -1,12 +1,18 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
+from flask_dotenv import DotEnv
 
 
 class BaseConfig(object):
     DEBUG = True
     TESTING = False
-    MACHINE_NAME = "SANDEEP"
+    MACHINE_NAME = "Complex Challenge"
     SECRET_KEY = os.environ.get("SECRET_KEY", "SECRETTZ")
-    DEVELOPER_ID = "8978548a-d17a-43a4-8d8c-2da6361b991a"
+    DEVELOPER_ID = os.environ.get('DEVELOPER_ID', '')
+
+    @classmethod
+    def init_app(self, app):
+        env = DotEnv()
+        env.init_app(app)
 
